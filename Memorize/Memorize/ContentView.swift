a5ba066dd9b9c25cc874d5290fab676d06e8ba82
Mohamed Sugal <sugaal@gmail.com>
2022-12-27 19:32:21 -0600

    Added emojis to cards game

--- Memorize/Memorize/ContentView.swift
+++ Memorize/Memorize/ContentView.swift
@@ -8,29 +8,45 @@
 import SwiftUI
 
 struct ContentView: View {
+    var emojis = ["🚊", "✈️", "🚌", "🚴‍♀️"]
+    
     var body: some View {
-        ZStack {
-            RoundedRectangle(cornerRadius: 25)
-                .stroke(lineWidth: 3)
-            Text("Hello World!")
-                .fontWeight(.bold)
+        HStack {
+            ForEach(emojis, id: \.self) {emoji in
+                CardView(content: emoji)
+            }
         }
         .padding(.horizontal)
-        .foregroundColor(.blue)
+        .foregroundColor(.red)
     }
 }
 
-
-
-
-
-
-
-
-
+struct CardView : View {
+    var content: String
+    @State var isFaceUp: Bool = true
+    
+    var body: some View {
+        let shape = RoundedRectangle(cornerRadius: 25)
+        ZStack {
+            if isFaceUp {
+                shape.fill().foregroundColor(.white)
+                shape.stroke(lineWidth: 3)
+                Text(content).font(.largeTitle)
+            } else {
+                shape.fill()
+            }
+        }
+        .onTapGesture {
+            isFaceUp = !isFaceUp
+        }
+    }
+}
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .preferredColorScheme(.dark)
+        ContentView()
+            .preferredColorScheme(.light)
     }
 }
